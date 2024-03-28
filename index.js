7bff433be79b56110aeb7f05f798d529fb155bce
pojojojo21 <jofisch@seas.upenn.edu>
2024-03-28 01:18:27 -0400

    Developed separated mobile and web style

--- index.js
+++ index.js
@@ -6,44 +6,15 @@ document.addEventListener('DOMContentLoaded', function () {
     let header = document.querySelector('header'); // Select the header element
     // let links = document.querySelectorAll('.nav-link');
 
-    navigation.addEventListener('click', function () {
-        navigating = !navigating;
-        navBar.classList.toggle('show');
-        header.classList.toggle('dark-bg');
+    // navigation.addEventListener('click', function () {
+    //     navigating = !navigating;
+    //     navBar.classList.toggle('show');
+    //     header.classList.toggle('dark-bg');
 
-        if (navigating) {
-            navigation.innerHTML = 'X';
-        } else {
-            navigation.innerHTML = '&#9776;';
-        }
-    });
-
-    // Add a scroll event listener to the window object
-    window.addEventListener('scroll', function () {
-        // Check if the scroll position is greater than 50px
-        if (window.scrollY > 50) {
-            // If it is, add the 'dark-bg' class to the header element
-            header.classList.add('dark-bg');
-        } else {
-            // Otherwise, remove the 'dark-bg' class from the header element
-            header.classList.remove('dark-bg');
-        }
-    });
-
-    // Select all the links in the navigation menu
-    // links.forEach(function (link) {
-    //     // Add a click event listener to each link
-    //     link.addEventListener('click', function () {
-    //         // Remove the 'show' class from the navigation menu
-    //         // navBar.classList.remove('show');
-    //         // header.classList.remove('dark-bg');
-    //         // navigation.innerHTML = '&#9776;';
-    //         // console.log('clicked');
-    //         links.forEach(function (el) {
-    //             el.classList.remove('active');
-    //         });
-    //         this.classList.add('active');
-    //     });
+    //     if (navigating) {
+    //         navigation.innerHTML = 'X';
+    //     } else {
+    //         navigation.innerHTML = '&#9776;';
+    //     }
     // });
-
 });
